82854ec53118dffdb0ea85350ad45df9d940ab24
Alan Cleyton <alancleyton67@gmail.com>
2023-03-11 11:23:29 -0300

    add ESlint and Prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import React, { type ReactElement } from 'react';
+
+function App(): ReactElement {
   return (
     <div id="App">
       <h1>Hello world!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
