850eb8fd644e9ff48169119572bdfb9d90971559
Alan Cleyton <alancleyton67@gmail.com>
2023-03-16 11:53:54 -0300

    add tailwindcss and postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import './App.css';
 import React, { type ReactElement } from 'react';
 
 function App(): ReactElement {
   return (
     <div id="App">
-      <h1 data-testid="title">Hello world!</h1>
+      <h1 data-testid="title" className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
